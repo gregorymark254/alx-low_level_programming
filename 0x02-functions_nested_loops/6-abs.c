fce16b538ad5b1aae7fe7ce75b3e450980bd28a1
gregorymark254 <gregorymark254@gmail.com>
2023-06-20 12:16:25 -0700

    nested loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - check main
+ * @n: input
+ * Description: function _abs()
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (n * -1);
+}
