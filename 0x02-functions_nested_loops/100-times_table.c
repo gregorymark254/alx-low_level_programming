318c595335f868c495047cd13147c1064d30c4a1
gregorymark254 <gregorymark254@gmail.com>
2023-06-20 12:51:36 -0700

    nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -9,46 +9,45 @@
 
 void print_times_table(int n)
 {
-	int i, j, val;
+	int row;
+	int column;
+	int product;
 
-	for (i = 0; i <= n; i++)
+	if (n >= 0 && n <= 15)
 	{
-		for (j = 0; j <= n; j++)
+		for (row = 0; row <= n; row++)
 		{
-			val = i * j;
-
-			if (val <= 9)
+			for (column = 0; column <= n; column++)
 			{
-				if (j != 0)
+				product = (row * column);
+				if (column == 0)
+					_putchar('0' + product);
+				else
+				{
+					_putchar(',');
 					_putchar(' ');
-				_putchar(val + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+					if (product <= 9)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar('0' + product);
+					}
+					else if (product > 9 && product < 100)
+					{
+						_putchar(' ');
+						_putchar('0' + (product / 10));
+						_putchar('0' + (product % 10));
+					}
+					else if (product >= 100)
+					{
+						_putchar('0' + (product / 100));
+						_putchar('0' + ((product / 10) % 10));
+						_putchar('0' + (product % 10));
+					}
+				}
 			}
-			else if (val > 9 && val <= 99)
-			{
-				_putchar((val / 10) + '0');
-				_putchar((val % 10) + '0');
-				if (j == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
-			else
-			{
-				_putchar((val / 100) + '0');
-				_putchar((val / 10 % 10) + '0');
-				_putchar((val % 10) + '0');
-				if(j == n)
-					continue;
-				_putchar(',');
-				_putchar(' ');
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
+}  
+
