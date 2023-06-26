08288706852d74878678e51da3322cd77b8f9132
gregorymark254 <gregorymark254@gmail.com>
2023-06-26 11:31:40 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - check main
+ * @s: input
+ * Description: function print_rev
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (len)
+		_putchar(s[--len]);
+	_putchar('\n');
+}
