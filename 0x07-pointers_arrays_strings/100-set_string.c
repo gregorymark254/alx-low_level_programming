9525cd1445da4925de656734c5fa12f58e5b4ad0
gregorymark254 <gregorymark254@gmail.com>
2023-07-03 12:51:26 -0700

    pointers

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * set_string - check main
+ * @s: input
+ * @to: input
+ * Description: function set_string
+ * Return: 0
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
