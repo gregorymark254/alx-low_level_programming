9052bb9005e27599f79d2db13dcec9cad431c3f1
gregorymark254 <gregorymark254@gmail.com>
2023-07-07 15:36:34 -0700

    arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+* @argc: input
+* @argv: input
+* Description: function
+* Return: 0
+*/
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
