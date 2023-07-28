a6e2e2f0fb43932ab0bfef3b67612b35fdcba92d
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 05:28:11 -0700

    list node

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -9,12 +9,15 @@ size_t print_list(const list_t *h)
 {
 	size_t c = 0;
 
+	if (h == NULL)
+		return (0);
+
 	while (h != NULL)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%f] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 		c++;
 		h = h->next;
 	}
