6c879342a6a8afefc4388cf9a067b6a0e72d08e1
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 05:37:40 -0700

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,24 +8,23 @@
  */
 
 list_t *add_node(list_t **head, const char *str)
-
 {
 	list_t *newnode;
-	int c = 0;
+	int length = 0;
 
-	addnode = malloc(sizeof(list_t));
+	newnode = (list_t *)malloc(sizeof(list_t));
 
-	while (str[c])
-		c++;
+	while (str[length])
+		length++;
 
-	if (addnode == NULL)
+	if (newnode == NULL)
 		return (NULL);
 
-	addnode->str = strdup(str);
-	addnode->next = *head;
-	addnode->len = c;
+	newnode->str = strdup(str);
+	newnode->next = *head;
+	newnode->len = length;
 	*head = newnode;
 
-	return (addnode);
+	return (newnode);
 
 }
