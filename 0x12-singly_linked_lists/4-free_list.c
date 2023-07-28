04511e6921798ac28655407063ea067efc3bde7f
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 05:02:47 -0700

    list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,19 @@
 #include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: the linked list to be freed
+ * Return: 0
+ */
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head->next;
+		free(head->str);
+		free(head);
+		head = current;
+	}
+}
