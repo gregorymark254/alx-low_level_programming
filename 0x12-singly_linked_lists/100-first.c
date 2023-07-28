71725f3d4f1a6140be63089e1b34d5d08e3e052b
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 06:11:16 -0700

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints a sentence before main
+ */
+
+void main(void)__attribute__((constructor));
+
+void main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
