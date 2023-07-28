7664e389516c5a10522252db6dcf53abdad8cdd3
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 04:55:41 -0700

    add nodes

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,43 +1,34 @@
 #include "lists.h"
 
 /**
- * add_node - adds a node at the beginning of the linked list
- *
- * @head: pointer to the first node of the list
- * @str: string to add to new node in the list
- *
- * Return: NULL if it fails / starting addr of the list
+ * add_node - adds a node
+ * @head: pointer to the node
+ * @str: string to add a node
+ * Return: NULL if it fails or address
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	size_t str_len = 0;
+	size_t c = 0;
 
 	if (str == NULL)
-		str_len = 0;
+		c = 0;
 
-	while (str[str_len] != '\0')
-		str_len++;
+	while (str[c] != '\0')
+		c++;
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-	/**
-	 * initialize the next addr of new_node
-	 * to NULL if head is NULL, if it's not
-	 * move the first node to the addr of new_node
-	 * ->next hence adding new_node at the beginning
-	 *  of the list
-	 */
 	if (*head == NULL)
 		new_node->next = NULL;
 	else
 		new_node->next = *head;
 
 	new_node->str = strdup(str);
-	new_node->len = str_len;
+	new_node->len = c;
 	*head = new_node;
 
 	return (*head);
