eaa4386f8dd13048585d7dd5da68d4648df2a3ad
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 03:34:44 -0700

    singly linked

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,3 +1,18 @@
 #include "lists.h"
 
+size_t list_len(const list_t *h)
+{
+	size_t node_count = 1;
 
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+
+		node_count++;
+	}
+
+	return (node_count);
+}
