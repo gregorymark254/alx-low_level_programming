3b3d9d9a0130bddc43c778d3d984e48bc229e9fe
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 06:18:53 -0700

    begin

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,7 +4,7 @@
  * begin - prints a sentence before main
  */
 
-void begin(void)
+void __attribute__ ((constructor)) begin()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
