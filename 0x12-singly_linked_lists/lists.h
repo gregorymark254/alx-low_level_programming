9bdff05a38f98667eed091ed6f0918a62e56d742
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 07:21:15 -0700

    list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -10,10 +10,11 @@
  * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
- *
  * Description: singly linked list node structure
- * for Holberton project
+ * for Alx africa project
  */
+
+
 typedef struct list_s
 {
 	char *str;
@@ -27,5 +28,6 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+
 
 #endif
