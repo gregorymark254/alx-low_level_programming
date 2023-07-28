c60fa23c1f499606649124431c57bb7e09db218a
gregorymark254 <gregorymark254@gmail.com>
2023-07-28 03:37:03 -0700

    singly

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ * list_len - gets the number of nodes
+ *
+ * @h: pointer to the head (first node)
+ *
+ * Return: number of nodes
+ */
+
 size_t list_len(const list_t *h)
 {
 	size_t node_count = 1;
