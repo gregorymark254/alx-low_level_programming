a295aa27e63bf89c2e00b9b384dbe930ef265bdd
gregorymark254 <gregorymark254@gmail.com>
2023-08-01 13:35:59 -0700

    singly list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,14 +1,1 @@
-#include "lists.h"
 
-/**
-  * print_listint_safe - print start of node
-  * @head: pointer to a node
-  * Return: null or address
-  */
-
-size_t print_listint_safe(const listint_t *head)
-
-{
-	head++;
-	return (0);
-}
