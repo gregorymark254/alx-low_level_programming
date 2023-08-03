173a888fde926af185cf988e2db3e95c8d55bbdd
gregorymark254 <gregorymark254@gmail.com>
2023-08-03 02:34:50 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,26 +8,4 @@
 
 void print_binary(unsigned long int n)
 
-{
-	int b = sizeof(unsigned long int) * 8, i;
-	unsigned long int m;
-	int post = 0;
 
-	for (i = b - 1; i >= 0; i--)
-	{
-		m = 1UL << i;
-		if ((n & m) == m)
-		{
-			_putchar('1');
-			post = 1;
-		}
-		else if (post == 1)
-		{
-			_putchar('0');
-		}
-	}
-	if (post == 0)
-	{
-		_putchar('0');
-	}
-}
