e0534a88b3b07e4968b02b9ccd579f38359943c8
gregorymark254 <gregorymark254@gmail.com>
2023-09-04 11:29:58 -0700

    files

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,20 +2,22 @@
 #include <stdio.h>
 
 /**
- * error_file - checks if files can be opened.
- * @file_from: file_from.
- * @file_to: file_to.
- * @argv: arguments vector.
- * Return: no return.
- */
-void error_file(int file_from, int file_to, char *argv[])
+ * file_error - check if there is errors in files
+ * @org: origin of the file
+ * @dest: destination of the file
+ * @argv: arguments
+ * Return: null or address
+*/
+
+void file_error(int org, int dest, char *argv[])
+
 {
-	if (file_from == -1)
+	if (org == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
+	if (dest == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
@@ -23,49 +25,51 @@ void error_file(int file_from, int file_to, char *argv[])
 }
 
 /**
- * main - check the code for ALX students.
- * @argc: number of arguments.
- * @argv: arguments vector.
- * Return: Always 0.
- */
+ * main - starting point
+ * @argc: argument counter
+ * @argv: arguments vector
+ * Return: null or address
+*/
+
 int main(int argc, char *argv[])
+
 {
-	int file_from, file_to, err_close;
+	int org, dest, close_error;
 	ssize_t nchars, nwr;
 	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp org dest");
 		exit(97);
 	}
 
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	org = open(argv[1], O_RDONLY);
+	dest = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	file_error(org, dest, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
-		nchars = read(file_from, buf, 1024);
+		nchars = read(org, buf, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
+			file_error(-1, 0, argv);
+		nwr = write(dest, buf, nchars);
 		if (nwr == -1)
-			error_file(0, -1, argv);
+			file_error(0, -1, argv);
 	}
 
-	err_close = close(file_from);
-	if (err_close == -1)
+	close_error = close(org);
+	if (close_error == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", org);
 		exit(100);
 	}
 
-	err_close = close(file_to);
-	if (err_close == -1)
+	close_error = close(dest);
+	if (close_error == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", org);
 		exit(100);
 	}
 	return (0);
